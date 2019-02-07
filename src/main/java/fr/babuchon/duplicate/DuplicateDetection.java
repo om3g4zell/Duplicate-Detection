886bc0ceb8580c0d38b26cfb0fbabb51efc25a82
Louisb <louis.babuchon@etu.univ-tours.fr>
2019-02-07 22:42:20 +0100

    Debut de l'implementation

---
+++ src/main/java/fr/babuchon/duplicate/DuplicateDetection.java
@@ -0,0 +1,34 @@
+package fr.babuchon.duplicate;
+
+import ij.ImagePlus;
+
+public class DuplicateDetection {
+
+    public float getDistWithSAD(ImagePlus image1, ImagePlus image2) {
+
+        int width1 = image1.getWidth();
+        int height1 = image1.getHeight();
+
+        int width2 = image2.getWidth();
+        int height2 = image2.getHeight();
+
+        if(width1 != width2 || height1 != height2) {
+            throw new IllegalArgumentException("Erreur les images doivent avoir les mêmes dimensions");
+        }
+
+        int sad = 0;
+        for(int i = 0 ; i < width1; i++) {
+            for(int j = 0 ; j < height1; j++) {
+                int pixel1 = image1.getPixel(i, j)[0];
+                int pixel2 = image2.getPixel(i, j)[0];
+
+                sad += Math.abs(pixel1 - pixel2);
+            }
+        }
+
+        // sad / something normalization
+
+        return sad;
+    }
+
+}
--- src/main/java/fr/babuchon/duplicate/Main.java
+++ src/main/java/fr/babuchon/duplicate/Main.java
@@ -4,17 +4,48 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
 
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     private static final String PATH = "D:\\Images\\PRD\\Doublon_exact_scale";
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-        LOGGER.info("Test logger");
 
-        File file = new File(PATH);
+        DuplicateDetection dd = new DuplicateDetection();
+
+        File folder = new File(PATH);
+        File files[] = folder.listFiles();
+
+        LOGGER.debug("lengh : {}", files.length);
+
+        int duplicateNumber = 0;
+        int differentNumber = 0;
+
+        for(int i = 0 ; i < files.length; i++) {
+            File currentFolder = files[i];
+            File[] different = currentFolder.listFiles(pathname -> {
+                if(pathname.getName().equals("different")) return true;
+                return false;
+            });
+            File[] duplicate = currentFolder.listFiles(pathname -> {
+                if(pathname.getName().equals("duplicate")) return true;
+                return false;
+            });
+            for(File f : different[0].listFiles()) {
+                differentNumber++;
+                LOGGER.debug(f.getName());
+            }
+            for(File f : duplicate[0].listFiles()) {
+                LOGGER.debug(f.getName());
+                duplicateNumber++;
+            }
+        }
+
+        LOGGER.info("Duplicate : {}, Different : {}", duplicateNumber, differentNumber);
+
+        /*File file = new File(PATH);
 
         for(int i = 0 ; i < 30; i++) {
             File f = new File(PATH, "" + i);
@@ -26,7 +57,9 @@ public class Main {
             File different = new File(PATH + "\\" + i, "different");
             different.mkdir();
             LOGGER.info(PATH + "\\" + i + "\\different");
-        }
+        }*/
+
+
 
     }
 }
